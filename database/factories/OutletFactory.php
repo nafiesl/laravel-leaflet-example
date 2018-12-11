05d12bb17a0acc04fc3a9fefec2d8be9c9e9547e
Nafies Luthfi <nafiesl@gmail.com>
2018-12-11 22:17:38 +0800

    Update name attribute on OutletFactory

--- database/factories/OutletFactory.php
+++ database/factories/OutletFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 $factory->define(Outlet::class, function (Faker $faker) {
 
     return [
-        'name'       => $faker->word,
+        'name'       => ucwords($faker->words(2, true)),
         'address'    => $faker->address,
         'latitude'   => $faker->latitude(-3.29, -3.35),
         'longitude'  => $faker->longitude(114.56, 114.63),
