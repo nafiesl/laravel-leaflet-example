a8a4ee52500dcce54a28f45b9c8c8179bedcdced
Nafies Luthfi <nafiesl@gmail.com>
2018-12-19 22:17:41 +0800

    Use env on map center coordinate config

--- config/leaflet.php
+++ config/leaflet.php
@@ -3,6 +3,6 @@
 return [
     'zoom_level'           => 13,
     'detail_zoom_level'    => 16,
-    'map_center_latitude'  => '-3.313695',
-    'map_center_longitude' => '114.590148',
+    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-3.313695'),
+    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '114.590148'),
 ];
